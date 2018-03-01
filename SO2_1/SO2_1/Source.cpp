a8c4041a91532524b117339908114c8f5ce1c4b6
g_milaszkiewicz <gmilaszkiewicz@gmail.com>
2018-03-01 22:54:35 +0100

    Test wątków

--- SO2_1/SO2_1/Source.cpp
+++ SO2_1/SO2_1/Source.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
+#include <thread>
 
 using namespace std;
 
+void call_from_thread(int tid) {
+	cout << "watek: " << tid << endl;
+}
+
+
 int main() {
+	thread t[5];
 
-	cout << "hello" << endl;
+	for (int i = 0; i < 5; i++)
+	{
+		t[i] = thread(call_from_thread, i);
+	}
 
+	cout << "main" << endl;
+
+	for (int i = 0; i < 5; i++)
+	{
+		t[i].join();
+	}
+
+	system("pause");
 	return 0;
 }
