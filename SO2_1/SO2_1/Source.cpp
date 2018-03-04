5b7516de227943621d08f7a05e109b50c364e651
g_milaszkiewicz <gmilaszkiewicz@gmail.com>
2018-03-04 12:16:06 +0100

    Dodanie cykli filozofowanie/jedzenie

--- SO2_1/SO2_1/Source.cpp
+++ SO2_1/SO2_1/Source.cpp
@@ -1,24 +1,37 @@
 #include <iostream>
 #include <thread>
+#include <time.h>
+#include <Windows.h>
 
 using namespace std;
 
+const int num_threads = 3;
+const int iloscCykli = 2;
+
 void call_from_thread(int tid) {
-	cout << "watek: " << tid << endl;
+	cout << "Filozof " << tid <<" zasiadl do stolu." <<endl;
+	for (int i = 0; i < iloscCykli; i++)
+	{
+		cout << "Filozof " << tid << " je." << endl;
+		int czasJedzenia = rand() % 500;
+		Sleep(czasJedzenia);	
+		cout << "Filozof " << tid << " filozofuje." << endl;
+		int czasFilozofowania = rand() % 500;
+		Sleep(czasFilozofowania);
+	}
 }
 
 
-int main() {
-	thread t[5];
+int main(){ 
+	thread t[num_threads];
 
-	for (int i = 0; i < 5; i++)
+	for (int i = 0; i < num_threads; i++)
 	{
 		t[i] = thread(call_from_thread, i);
 	}
-
+	
 	cout << "main" << endl;
-
-	for (int i = 0; i < 5; i++)
+	for (int i = 0; i < num_threads; i++)
 	{
 		t[i].join();
 	}
